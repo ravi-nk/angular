108f6b9bb6c9bb0244f0349722b4cef7924138b0
ravinsan <ravichoudhary33366@gmail.com>
2020-04-08 09:35:52 +0530

    design in angular in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bank';
+  myFunctions(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+  
+   myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  
+  
+  }
+  
+  
+  //  ac(){
+  //  if(do('#color').hasClass('active')){
+  
+  //   $('#color').removeClass('active');
+  //   }
+  //   else
+  //   {
+  //   $('#color').addClass('active');
+  //   }
+  // }
 }
